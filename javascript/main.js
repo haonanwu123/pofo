0d097b4e2f753861fd244c4001dfb3e238ff44b7
haonanwu123 <92578731+haonanwu123@users.noreply.github.com>
2022-03-09 10:05:25 +0100

    pofo

--- javascript/main.js
+++ javascript/main.js
@@ -1,10 +1,17 @@
-const modal = document.querySelector("#modal");
-const bgImg = document.getElementById("beImg");
-const project1 = document.querySelector("#project-js")
-
-function showBgImg(e) {
-    modal.style.display = "block";
-    bgImg.src = e.src;
+let flag = true,
+             imgH,
+             imgW,
+             img = document.getElementsByClassName("project-img")[2];
+img.onclick = function() {
+    imgH = img.height;
+    imgW = img.width;
+    if(flag){
+        flag = false;
+        img.height = imgH * 2;
+        img.width = imgW * 2;
+    }else{
+        flag = true;
+        img.height = imgH / 2;
+        img.width = imgW / 2;
+    }
 }
-
-project1.addEventListener("click", showBgImg)
